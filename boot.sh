0bb80ede1e5e7d75fc83255177e2a7575ad26056
xiesenxin <xiesenxin@bytedance.com>
2022-06-12 20:53:34 +0800

    添加启动脚本

---
+++ boot.sh
@@ -0,0 +1,6 @@
+#=======================================
+#  @FileName: start.sh
+#  @Author: 庄海铃
+#  @Date: 2022年06月12日
+#=======================================
+cd src && javac -d ../output main.java && cd ../output && java main
