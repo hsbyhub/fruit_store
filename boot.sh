6f9a39677176fd69b963fa239222e7b40ba1bd74
xiesenxin <xiesenxin@bytedance.com>
2022-07-24 16:15:00 +0800

    调整boot

--- boot.sh
+++ boot.sh
@@ -1,6 +1,1 @@
-#=======================================
-#  @FileName: boot.sh
-#  @Author: 庄海铃
-#  @Date: 2022年06月12日
-#=======================================
 cd src && javac -d ../output main.java && cd ../output && java main
